b6cb65e132ac39ff7d61633fd78077f29aecff5e
Keian Gacillos <gacilloskeian02@gmail.com>
2026-04-15 23:31:56 +0800

    fixing the title and adding an icon logo

--- admin/complaints.php
+++ admin/complaints.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Complaints — ESAMELCO</title>
+    <title>ESAMELCO - Customer Complaint Monitoring and Management System</title>
 
+    <link rel="icon" href="../assets/img/logo.jpg">
     <!-- Bootstrap 5 CSS -->
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <!-- Bootstrap Icons -->
