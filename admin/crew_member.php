3f0799b5bf8da63fb3dd9d8a12eb54b05e53b734
Keian Gacillos <gacilloskeian02@gmail.com>
2026-04-17 23:33:56 +0800

    fixex my crew member php

--- admin/crew_member.php
+++ admin/crew_member.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../assets/css/admin_dashboard.css">
+    <link rel="stylesheet" href="../assets/css/admin_crew_member.css">
 
 </head>
 <body>
